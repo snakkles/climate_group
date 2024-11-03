bd30de1f6c34880de7d54537289db0cd364bdcfb
Alex Fell <alex.fell@ggmr.co.uk>
2024-11-03 19:55:05 +0000

    Adjusted heat/idle logic to my taste

--- custom_components/climate_group/climate.py
+++ custom_components/climate_group/climate.py
@@ -283,14 +283,12 @@ class ClimateGroup(GroupEntity, ClimateEntity):
         hvac_actions = list(find_state_attributes(states, ATTR_HVAC_ACTION))
         if hvac_actions:
             current_hvac_actions = [a for a in hvac_actions if a != HVACAction.OFF]
-            # return Idle if all idle, or heat if one or more is heat
+            # return HVACAction.HEATING if any set to heat, or most common otherwise
             if current_hvac_actions:
-                if all(a == HVACAction.IDLE for a in current_hvac_actions):
-                    self._attr_hvac_action = HVACAction.IDLE
-                elif any(a == HVACAction.HEATING for a in current_hvac_actions):
+                if any(a == HVACAction.HEATING for a in current_hvac_actions): 
                     self._attr_hvac_action = HVACAction.HEATING
                 else:
-                    # set the most common
+                    # set the most common action
                     self._attr_hvac_action = max(set(current_hvac_actions), key=current_hvac_actions.count)
             # return HVACAction.OFF if all actions are set to off
             elif all(a == HVACAction.OFF for a in hvac_actions):
